de9a99a5378805126f0a2056954ae896186ea009
steven <steventwig@gmail.com>
2019-10-25 05:22:23 -0700

    set up projects page and did basic inline css

--- react-porfolio/src/components/projects.js
+++ react-porfolio/src/components/projects.js
@@ -1,10 +1,104 @@
 import React, {Component}from 'react';
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText} from "react-mdl";
 
 class Projects extends Component {
+    constructor(props){
+        super(props);
+        this.state = {activeTab: 0};
+    }
+    toggleCatagories(){
+        if(this.state.activeTab === 0){
+            return (
+                <div className="projects-grid">
+                <Card shadow={5} style={{width: '450px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>React</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Git Hub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        }else if (this.state.activeTab === 1){
+            return (
+                <div className="projects-grid">
+                    <Card shadow={5} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: 'black', height: '176px', background: 'url(https://www.pubnub.com/wp-content/uploads/2014/07/SOCKETIOICON.gif) center / cover'}}>Socket</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Git Hub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        }else if(this.state.activeTab === 2){
+            return (
+                <div className="projects-grid">
+                    <Card shadow={5} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>React</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Git Hub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        }else if(this.state.activeTab === 3){
+            return (
+                <div className="projects-grid">
+                    <Card shadow={5} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>React</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Git Hub</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        }
+    }
+
+
+
     render() {
         return (
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Socket</Tab>
+                    <Tab>MySQL</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCatagories()}</div>
+                        </Cell>
+                    </Grid>
             </div>
         );
     }
